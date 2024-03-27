55ba4edb1889294641e35f77436d3f83e9be4cac
VeganMeatGuy <steudel.peter@gmail.com>
2024-03-27 20:55:22 +0100

    add EFCore package and Domain Classes

---
+++ Learning.EFCore/Learning.EFCore/Domain/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.EFCore.Domain;
+
+public class Student
+{
+    public int StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public int GradeId { get; set; }
+    public Grade Grade { get; set; }
+}
